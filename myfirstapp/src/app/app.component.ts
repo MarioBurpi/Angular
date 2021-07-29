032d7dc730a9a299e4401a85ff06658792fc9059
Mario Burgos <a19marburpie@inspedralbes.cat>
2021-07-29 14:18:17 +0200

    creación de componentes empleados y empleado.
    ** creación de componentes inline **
    
    ng g c componente -s -t  (-s y -t para indicar style y template)

--- myfirstapp/src/app/app.component.ts
+++ myfirstapp/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'myapp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//En esta clase se definen todos los componentes (reutilizables) que se quiere usar en el proyecto
 export class AppComponent {
   title = 'myfirstapp';
+  helloWorld = "Hello World!"
 }
