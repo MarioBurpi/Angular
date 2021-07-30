250a535fec156618d5bda7b264759fa0e6829daf
Mario Burgos <a19marburpie@inspedralbes.cat>
2021-07-30 15:56:20 +0200

    directiva ngFor

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -11,6 +11,18 @@ export class AppComponent {
   registrado = false;
   nombre:string = "";
   apellido:string = "";
+  entradas: any;
+
+  constructor(){
+    this.entradas = [
+      {titulo:"La importancia del ROM"}, 
+      {titulo:"Haz tus push ups más efectivas"}, 
+      {titulo:"Progresión: destino front lever"}, 
+      {titulo:"Descansos, ¿Cómo calcularlos?"}, 
+      {titulo:"Progresión: Muscle Up"},
+    ]
+  }
+  
 
   registrarUsuario():void{
     this.registrado = true;
