c3d0ff9597c56c52ecf4aebcda90f875de59c62e
Mario Burgos <a19marburpie@inspedralbes.cat>
2021-07-30 17:56:14 +0200

    Formulario que utiliza las herramientas y estructuras aprendidas hasta el momento.

---
+++ empleados-app/src/app/empleado.model.ts
@@ -0,0 +1,17 @@
+export class Empleado {
+
+    //Attributes
+    nombre:string = "";
+    apellido:string = "";
+    cargo:string = "";
+    salario:number = 0;
+
+    //Constructor
+    constructor(nombre:string, apellido:string, cargo:string, salario:number){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+}
