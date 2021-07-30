6a2d778d8cd1a28ab74a8c62b9b2c42bbb39c5ef
Mario Burgos <a19marburpie@inspedralbes.cat>
2021-07-30 14:13:05 +0200

    Calculadora muy simple que suma y resta

--- Calculadora-Angular/src/app/app.component.ts
+++ Calculadora-Angular/src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Calculadora-Angular';
+  //Atributos de clase
+  title = 'Calculadora con Angular';
+  num1:number = 0;
+  num2:number = 0;
+  result:number = 0;
+  
+  //Metodos
+  sumar():void{
+    this.result = this.num1 + this.num2;
+  }
+  restar():void{
+    this.result = this.num1 - this.num2;
+  }
+
 }
